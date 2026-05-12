bcee5b37a6206bd2f2a10bd3ea3b58e7f5914196
Divyanshu Sahu <257divyanshu@gmail.com>
2026-05-12 18:18:59 +0530

    feat: create movie schema and seed sample movie data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const env = require('dotenv');
 const mongoose = require('mongoose');
+const Movie = require('./models/movie.model');
 
 env.config();
 const app = express(); // express() returns an express application object
@@ -11,26 +12,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/home', (req, res) => {
-
-    // req is the request object that client will send
-    // res is the response object that we will send
-
     console.log("Hitting /home");
     return res.json({
         success: true,
         message: 'Fetched home'
     });
-    // 'return res.json({...})' creates json inside the response object and returns the response object
 });
 
 app.listen(process.env.PORT, async () => {
-    // this callback gets execcuted, once we successfully start the server on the given port
+    // this callback gets executed, once we successfully start the server on the given port
     console.log(`Server started on Port ${process.env.PORT}`);
 
     try {
         await mongoose.connect(process.env.DB_URL); // connect to the mongodb server
-        console.log("Successfully connected to mongodb server");
+        console.log("Successfully connected to mongodb");
+        await Movie.create({
+            name: "Bacchan Pandey",
+            description: "Comedy masala movie",
+            casts: ["Akshay Kumar", "Kriti Sanon", "Jaqueline Fernandiz"],
+            director: "Farhad Samji",
+            trailerUrl: "http://bacchanpandey/trailers/1",
+            language: "Hindi",
+            releaseDate: "18-03-2022",
+            releaseStatus: "RELEASED"
+        });
+
     } catch (err) {
-        console.log("Not able to connect mongodb server", err);
+        console.log("Not able to connect to mongodb", err);
     }
 });
