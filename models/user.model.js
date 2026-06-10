8476beb41a47b45ee94150c2a61ec8e5de079087
Divyanshu Sahu <257divyanshu@gmail.com>
2026-06-10 18:06:01 +0530

    feat: hash user passwords before saving to database

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,6 +31,13 @@ const userSchema = new mongoose.Schema({
         default: "APPROVED"
     }
 }, {timestamps: true});
+
+userSchema.pre('save', async function () {
+    // a trigger to encrypt the plain password before saving the user
+    if (!this.isModified('password')) return;
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
