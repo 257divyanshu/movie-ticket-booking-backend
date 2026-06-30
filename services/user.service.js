29d3ef1f3476de6c8410c5997615f87a0dec464e
Divyanshu Sahu <257divyanshu@gmail.com>
2026-06-30 14:21:41 +0530

    feat: add role based user status validation during signup

--- services/user.service.js
+++ services/user.service.js
@@ -1,11 +1,24 @@
 const User = require('../models/user.model');
+const { USER_ROLE, USER_STATUS} = require('../utils/constants');
 
 const createUser = async (data) => {
     try {
+        if (!data.userRole || data.userRole == USER_ROLE.customer) {
+            if (data.userStatus && data.userStatus != USER_STATUS.approved) {
+                throw {
+                    err: "We cannot set any other status for customer",
+                    code: 400
+                };
+            }
+        }
+        if (data.userRole && data.userRole != USER_ROLE.customer) {
+            data.userStatus = USER_STATUS.pending;
+        }
         const response = await User.create(data);
         return response;
     } catch (error) {
         console.log('service layer error');
+        console.log(error);
 
         if (error.name == 'ValidationError') {
             let err = {};
@@ -14,7 +27,7 @@ const createUser = async (data) => {
             });
 
             console.log(err);
-            
+
             // old way (the way we have been doing)
             // return { err: err, code: 422 };
             // new way (throwing an error)
