e7029f9cee4523400ccfb136d6afe595c06a297f
Divyanshu Sahu <257divyanshu@gmail.com>
2026-07-01 17:58:41 +0530

    feat: add request validation middleware for user sigin API

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,26 +2,53 @@ const { errorResponseBody } = require('../utils/responsebody');
 
 const validateSignupRequest = async (req, res, next) => {
     // validate the presence of name
-    if(!req.body.name) {
+    if (!req.body.name) {
         const badRequestResponse = errorResponseBody();
         badRequestResponse.message = "Malformed Request | Bad Request";
-        badRequestResponse.err = "Name of the user is not present in the request";
+        badRequestResponse.err.message = "Name of the user is not present in the request";
         return res.status(400).json(badRequestResponse);
     }
 
     // validate the presence of email
-    if(!req.body.email) {
+    if (!req.body.email) {
         const badRequestResponse = errorResponseBody();
         badRequestResponse.message = "Malformed Request | Bad Request";
-        badRequestResponse.err = "Email of the user is not present in the request";
+        badRequestResponse.err.message = "Email of the user is not present in the request";
         return res.status(400).json(badRequestResponse);
     }
-    
+
     // validate the presence of password
-    if(!req.body.password) {
+    if (!req.body.password) {
         const badRequestResponse = errorResponseBody();
         badRequestResponse.message = "Malformed Request | Bad Request";
-        badRequestResponse.err = "Password of the user is not present in the request";
+        badRequestResponse.err.message = "Password of the user is not present in the request";
+        return res.status(400).json(badRequestResponse);
+    }
+
+    // request is valid
+    next();
+}
+
+/**
+ * validator for user signin
+ * @param req -> http request object
+ * @param res -> http response object
+ * @param next -> next middleware
+ */
+const validateSigninRequest = async (req, res, next) => {
+    // validate the presence of email
+    if (!req.body.email) {
+        const badRequestResponse = errorResponseBody();
+        badRequestResponse.message = "Malformed Request | Bad Request";
+        badRequestResponse.err.message = "No email provided for sign in";
+        return res.status(400).json(badRequestResponse);
+    }
+
+    // validate the presence of password
+    if (!req.body.password) {
+        const badRequestResponse = errorResponseBody();
+        badRequestResponse.message = "Malformed Request | Bad Request";
+        badRequestResponse.err.message = "No password provided for sign in";
         return res.status(400).json(badRequestResponse);
     }
 
@@ -30,5 +57,6 @@ const validateSignupRequest = async (req, res, next) => {
 }
 
 module.exports = {
-    validateSignupRequest
+    validateSignupRequest,
+    validateSigninRequest
 }
